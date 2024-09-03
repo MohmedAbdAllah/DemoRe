829bc4ba3f5b456ccd288aaecce2e4da4e626407
MohmedAbdAllah <eng.mohmed.abdallah@outlook.com>
2024-09-03 14:08:44 +0300

    add git action file

--- tests/test_homepage.py
+++ tests/test_homepage.py
@@ -4,4 +4,4 @@ def test_index():
     with app.test_client() as test_client:
         response = test_client.get("/")
         assert response.status_code == 200
-        #assert b"Hello" in response.data
+        assert b"Hello" in response.data
